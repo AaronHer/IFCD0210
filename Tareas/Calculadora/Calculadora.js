572c5310ade799297064578931d7ccaec38d4871
Aaron <aaron_pto@hotmail.com>
2019-02-18 19:06:10 +0000

    no message

---
+++ Tareas/Calculadora/Calculadora.js
@@ -0,0 +1,11 @@
+
+function addValor(valor) {
+    document.getElementById("display").value += valor;
+}
+function Limpar() {
+    document.getElementById("display").value = '';
+}
+function Calcular() {
+    var valor = document.getElementById("display").value;
+    document.getElementById("display").value = eval(valor);
+}
