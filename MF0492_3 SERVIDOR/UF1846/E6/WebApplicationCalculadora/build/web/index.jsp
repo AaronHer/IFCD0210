ba44449319d90fe32f04d5954473805cbb3520bb
Aaron <aaron_pto@hotmail.com>
2019-05-28 18:02:21 +0100

    no message

--- MF0492_3 SERVIDOR/UF1846/E6/WebApplicationCalculadora/build/web/index.jsp
+++ MF0492_3 SERVIDOR/UF1846/E6/WebApplicationCalculadora/build/web/index.jsp
@@ -25,6 +25,46 @@
         <input type="submit" value="Restar" name="botonRestar" />
         <input type="submit" value="Dividir" name="botonDividir" />
         <input type="submit" value="Multiplicar" name="botonMultiplicar" />
+        <input type="submit" value="Hipotenusa" name="botonHipo" />
+
         </form>
+        
+        <br><br>
+            
+        <h1>Comparador el resultado mayor</h1>
+        <form mathod="POST" action="SerbletOperaciones">
+        <br>
+        <p>Valor 1 : <input type="text" name="comparar1" value="" size="8" /></p>
+        <br>
+        <p>Valor 2 : <input type="text" name="comparar2" value="" size="8" /></p>
+        <input type="submit" value="Mayor" name="botonComparar" />
+        </form>
+        
+        <h1>Calcular Sueldo</h1>
+        <form mathod="POST" action="SerbletOperaciones">
+        <br>
+        <table border="0">
+            
+                <tr>
+                    <td>Nombre</td>
+                    <td><input type="text" name="Nombre" placeholder="Nombre" size="50" /></td>
+                </tr>
+                <tr>
+                    <td>Dirección</td>
+                    <td><input type="text" name="Dirección" placeholder="Dirección" size="50" /></td>
+                </tr>
+                <tr>
+                    <td>Dias trabajados</td>
+                    <td><input type="text" name="Dias" size="50" /></td>
+                </tr>
+                <tr>
+                    <td>Valor/hora</td>
+                    <td><input type="text" name="Valor" size="50" /></td>
+                </tr>
+        </table>
+        <br>
+        <input type="submit" value="Calcular" name="botonCalcular" />
+        </form>
+        
     </body>
 </html>
--- MF0492_3 SERVIDOR/UF1846/E6/WebApplicationCalculadora/src/java/aplicacion/selvlet/MiServlet.java
+++ MF0492_3 SERVIDOR/UF1846/E6/WebApplicationCalculadora/src/java/aplicacion/selvlet/MiServlet.java
@@ -7,11 +7,15 @@ package aplicacion.selvlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.ResultSet;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+//import AccesoDatos.AccesoBBDD;
+//import AccesoDatos.CalcularSueldo;
+import javax.servlet.RequestDispatcher;
 
 /**
  *
@@ -32,21 +36,31 @@ public class MiServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        //Creamos los abjetos para calcular el sueldo; de esta manera podemos usar los setter y getter de la clase java.
+
         try (PrintWriter out = response.getWriter()) {
+           
             /* TODO output your page here. You may use following sample code. */
+            
+            // * PARTE DE QUE CONECTA CON LA CALCULADORA * 
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Mi Calculadora</title>");            
+            out.println("<title>Mi Calculadora</title>"); 
+            out.println("<link href=newcss.css rel=stylesheet type= text/css>");
             out.println("</head>");
             out.println("<body>");
             
             String val1 = request.getParameter("valor1");
             String val2 = request.getParameter("valor2");
+            
+            
             String btnSumar = request.getParameter("botonSumar");
             String btnRestar = request.getParameter("botonRestar");
             String btnDividir = request.getParameter("botonDividir");
             String btnMultiplicar = request.getParameter("botonMultiplicar");
+            String btnHipo = request.getParameter("botonHipo");
+            
             
             
             if(btnSumar != null){
@@ -69,11 +83,55 @@ public class MiServlet extends HttpServlet {
             int resultado = Integer.parseInt(val1) * Integer.parseInt(val2); 
             out.println("El resultado es: " + resultado);
             }
+            if(btnHipo != null){
+                int cateto1 = Integer.parseInt(val1) * Integer.parseInt(val1);
+                int cateto2 = Integer.parseInt(val2) * Integer.parseInt(val2); 
+                int catetos = cateto1 + cateto2;
+                float resultado = (float) Math.sqrt(catetos); 
+            out.println("El resultado es: " + resultado);
+            }
             
 
 //out.println("<h1>Servlet MiServlet at " + request.getContextPath() + "</h1>");
+            
+            
+            
+        
+            String comparador1 = request.getParameter("comparar1");
+            String comparador2 = request.getParameter("comparar2");
+            String btnComparar = request.getParameter("botonComparar");
+            
+            if(btnComparar != null){
+                //comparador1.compareTo(comparador2);
+                
+                int valor1 = Integer.parseInt(comparador1);
+                int valor2 = Integer.parseInt(comparador2);
+                int resultado = 0;
+             
+                if(valor1<valor2){
+                    resultado = valor2;
+                }else  if(valor1>valor2){
+                    resultado = valor1;
+                }
+            out.println("El resultado es: " + resultado);
+            }
+            
+            String nom = request.getParameter("Nombre");
+            String dierccion = request.getParameter("Dirección");
+            String dia = request.getParameter("Dias");
+            String valor = request.getParameter("Valor");
+            String btnCalcular = request.getParameter("botonCalcular");
+            
+             if(btnCalcular != null){
+                int sueldoDia = Integer.parseInt(valor)*8;
+                int resultado = Integer.parseInt(dia) * sueldoDia; 
+                    out.println("Nombre: " + nom + "<br>" );
+                    out.println("Dirección: " + dierccion + "<br>" );
+                    out.println("Sueldo al mes: " + resultado + "€" );
+            }
+             
             out.println("</body>");
-            out.println("</html>");
+            out.println("</html>"); 
         }
     }
 
